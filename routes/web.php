1b2c5d16acbfb90d516ef6b8d4f6604e804618fc
Alechou Ezechiel <alechoue@gmail.com>
2025-01-05 21:53:00 +0100

    [Notes] reglage2?

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
 Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
 Route::post('/notes', [NoteController::class, 'store']);
 
